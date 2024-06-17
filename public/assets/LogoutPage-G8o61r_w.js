ef06742275cc027050b5d38bef5648d62bb98f64
User_Name <your_email@example.com>
2024-06-17 22:08:45 +0500

    update

---
+++ public/assets/LogoutPage-G8o61r_w.js
@@ -0,0 +1,1 @@
+import{f as a,g as n,j as o,S as r}from"./index-2CNeoBi0.js";import{B as i}from"./button-98tnv5Tm.js";const d=()=>{const e=a(),t=n.useSetUser();e!=null&&e.name||(window.location.pathname="/");const s=()=>{t(void 0),localStorage.removeItem("wedd-app")};return o.jsx(r,{$height:"100vh",children:o.jsx(i,{onClick:s,children:"Выйти из аккаунта"})})};export{d as default};
--- public/assets/LogoutPage-kY0MloHS.js
+++
@@ -1,1 +0,0 @@
-import{q as a,s as n,j as o,S as r}from"./index-pQZNF0D4.js";import{B as i}from"./button-CjjzQqAU.js";const d=()=>{const e=a(),t=n.useSetUser();e!=null&&e.name||(window.location.pathname="/");const s=()=>{t(void 0),localStorage.removeItem("wedd-app")};return o.jsx(r,{$height:"100vh",children:o.jsx(i,{onClick:s,children:"Выйти из аккаунта"})})};export{d as default};
